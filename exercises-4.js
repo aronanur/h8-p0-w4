d0ebee08a487f8793a42e9be8815acdd59f120b5
Arona Nur Tetulis <arona.nur.tetulis@gmail.com>
2019-12-04 22:16:47 +0700

    Perbaikan modus

--- exercises-4.js
+++ exercises-4.js
@@ -15,7 +15,6 @@ function cariModus(arr) {
     }
   }
 
-  console.log(modusArray)
   if(modusArray.length <= 1){
     return -1
   }
@@ -43,8 +42,8 @@ function cariModus(arr) {
 
 // TEST CASES
 console.log(cariModus([10, 4, 5, 2, 4])); // 4
-// console.log(cariModus([5, 10, 10, 6, 5])); // 5
-// console.log(cariModus([10, 3, 1, 2, 5])); // -1
-// console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
-// console.log(cariModus([7, 7, 7, 7, 7])); // -1
-// console.log(cariModus([1,2,3,4,6,7,8,5,4,3,2,9,2,3,4,5]))
+console.log(cariModus([5, 10, 10, 6, 5])); // 5
+console.log(cariModus([10, 3, 1, 2, 5])); // -1
+console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
+console.log(cariModus([7, 7, 7, 7, 7])); // -1
+console.log(cariModus([1,2,3,4,6,7,8,5,4,3,2,9,2,3,4,5]))
